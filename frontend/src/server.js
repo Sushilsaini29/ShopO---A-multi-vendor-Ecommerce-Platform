86fe1257ff6c86c0f8c177c4704a05ba01a77f90
sushil saini <sushilsaini2829@gmail.com>
2024-03-06 00:02:08 +0530

    URL deleted

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
 export const server = "https://shop-o-a-multi-vendor-ecommerce-platform.vercel.app/api/v2";
-export const backend_url = "https://shop-o-a-multi-vendor-ecommerce-platform.vercel.app";
+
 
 
