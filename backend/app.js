493c6082b6f72bd827cdedb3be5248b12c4581cd
sushil saini <sushilsaini2829@gmail.com>
2024-03-05 22:20:33 +0530

    my changes

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'https://shopo-dusky.vercel.app',
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 
